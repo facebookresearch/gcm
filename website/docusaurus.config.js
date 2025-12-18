492a5033cd7b1cfe4153d4bff616fab08b3dff9f
luccabb <luccab@meta.com>
2025-12-18 11:01:44 -0800

    update docusaurus baseurl

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -46,8 +46,7 @@ const config = {
   url: 'https://facebookresearch.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  // baseUrl: '/gcm',
-  baseUrl: '/',
+  baseUrl: '/gcm',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
